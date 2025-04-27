8254530605915f83725f2816a69063138ff30375
Allyssa <allyssalvacion@gmail.com>
2025-04-28 00:04:59 +0800

    Implement auth

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router';
-import { cn, getFirstWord } from 'lib/utils';
+import { cn, getFirstWord } from '~/lib/utils';
 import {
   ChipDirective,
   ChipListComponent,
