ae5f6eb50b05e00f4627fc7394816694fa9e222a
Allyssa <allyssalvacion@gmail.com>
2025-04-28 23:32:55 +0800

    Implement auth UI

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,14 +1,20 @@
 import { Header, StatsCard, TripCard } from 'components';
-import { dashboardStats, user, allTrips } from '~/constants';
+import { getUser } from '~/appwrite/auth';
+import { dashboardStats, allTrips } from '~/constants';
+import type { Route } from './+types/dashboard';
 
 const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } =
   dashboardStats;
 
-const Dashboard = () => {
+export const clientLoader = async () => await getUser();
+
+const Dashboard = ({ loaderData }: Route.ComponentProps) => {
+  const user = loaderData as User | null;
+
   return (
     <main className='dashboard wrapper'>
       <Header
-        title={`Welcome ${user?.name ?? 'Guest'}`}
+        title={`Welcome, ${user?.name ?? 'Guest'}! 👋🏼`}
         description='Track activity, trends, and popular destinations in real time'
       />
       <section className='flex flex-col gap-6'>
--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,12 +1,15 @@
 import { cn } from '~/lib/utils';
-import { Link, NavLink } from 'react-router';
+import { Link, NavLink, useLoaderData, useNavigate } from 'react-router';
 import { sidebarItems } from '~/constants';
+import { logoutUser } from '~/appwrite/auth';
 
 const NavItems = ({ handleClick }: { handleClick?: () => void }) => {
-  const user = {
-    name: 'Allyssa',
-    email: 'allyssalvacion@gmail.com',
-    imageUrl: '/assets/images/david.webp',
+  const user = useLoaderData();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await logoutUser();
+    navigate('/sign-in');
   };
 
   return (
@@ -43,18 +46,14 @@ const NavItems = ({ handleClick }: { handleClick?: () => void }) => {
         <footer className='nav-footer'>
           <img
             src={user?.imageUrl || '/assets/images/david.webp'}
-            alt={user?.name || 'Allyssa'}
+            alt={user?.name || 'Guest'}
+            referrerPolicy='no-referrer'
           />
           <article>
             <h2>{user?.name}</h2>
             <p>{user?.email}</p>
           </article>
-          <button
-            onClick={() => {
-              console.log('logout');
-            }}
-            className='cursor-pointer'
-          >
+          <button onClick={handleLogout} className='cursor-pointer'>
             <img
               src='/assets/icons/logout.svg'
               alt='logout'
