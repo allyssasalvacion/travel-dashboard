77a61b5862b5aae2e5417e7d6bd58d9f1d8d9f47
Allyssa <allyssalvacion@gmail.com>
2025-07-15 15:58:34 +0800

    Remove redirect

--- app/routes/root/admin-layout.tsx
+++ app/routes/root/admin-layout.tsx
@@ -4,20 +4,20 @@ import { Loading, MobileSidebar, NavItems } from 'components';
 import { getExistingUser, storeUserData } from '~/appwrite/auth';
 import { account } from '~/appwrite/client';
 
-export async function clientLoader() {
-  try {
-    const user = await account.get();
+// export async function clientLoader() {
+//   try {
+//     const user = await account.get();
 
-    if (!user.$id) return redirect('/sign-in');
+//     if (!user.$id) return redirect('/sign-in');
 
-    const existingUser = await getExistingUser(user.$id);
+//     const existingUser = await getExistingUser(user.$id);
 
-    return existingUser?.$id ? existingUser : await storeUserData();
-  } catch (e) {
-    console.log('Error fetching user', e);
-    return redirect('/sign-in');
-  }
-}
+//     return existingUser?.$id ? existingUser : await storeUserData();
+//   } catch (e) {
+//     console.log('Error fetching user', e);
+//     return redirect('/sign-in');
+//   }
+// }
 
 const AdminLayout = () => {
   const navigation = useNavigation();
